291a5058cb1748822f615bb8ea7f5d54575629e8
Carl Raymond <carl@orcharddev.com>
2012-10-10 18:11:04 -0400

    Added hub. No worky.

---
+++ Code/Website/Hubs/Tracker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SignalR.Hubs;
+
+namespace Website.Hubs
+{
+	public class Tracker : Hub
+	{
+		public void Send(string message)
+		{
+			Clients.addMessage(message);
+		}
+	}
+}
