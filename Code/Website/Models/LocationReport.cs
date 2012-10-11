d6d630f73548cf4ed458c6676d1c0efc816d74bb
CarlRaymond <carl@orcharddev.com>
2012-10-11 10:33:07 -0400

    Added LocationReport

---
+++ Code/Website/Models/LocationReport.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Website.Models
+{
+	public class LocationReport
+	{
+		public double Latitude { get; set; }
+		public double Longitude { get; set; }
+		public int Accuracy { get; set; }
+		public DateTime Timestamp { get; set; }
+		public string ID { get; set; }
+	}
+}
